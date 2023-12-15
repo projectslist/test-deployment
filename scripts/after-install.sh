ca2c36868223403bb21c7e29b6160df0a731b3f7
Yasin <67076345+projectslist@users.noreply.github.com>
2023-12-15 16:32:56 +0000

    Update after-install.sh

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Change directory to the project directory with the full path
+cd /home/ubuntu/test_project || exit 1
+
+# Ensure the script has execute permissions
+chmod +x /opt/codedeploy-agent/deployment-root/79e5729e-d804-48b6-bae7-d6c0818b25fb/d-KUT35NR61/deployment-archive/scripts/after-install.sh
+
 # Function to check if a command is available
 command_exists() {
   command -v "$1" >/dev/null 2>&1
@@ -20,8 +26,5 @@ install_docker_compose
 # Print the PATH for debugging
 echo "Current PATH: $PATH"
 
-# Change directory to the project directory with the full path
-cd /home/ubuntu/test_project || exit 1
-
-# Run the entire script with an explicit PATH
-PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/usr/local/bin:$PATH" ./scripts/after-install.sh
+# Use the dynamically found docker-compose path to build Docker containers
+docker-compose -f /home/ubuntu/test_project/docker_compose_prod.yml build --no-cache
