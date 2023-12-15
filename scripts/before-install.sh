32fd8b9daf5e3a30e08c6d87242cdccfffd11a3b
projectslist <67076345+projectslist@users.noreply.github.com>
2023-12-15 11:31:38 +0000

    update

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+# Install Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+# Add the ubuntu user to the docker group to run docker commands without sudo
+sudo usermod -aG docker ubuntu
+
+# Remove any existing test_project directory
 rm -rf /home/ubuntu/test_project
--- scripts/boot.sh
+++ scripts/boot.sh
@@ -1,5 +1,3 @@
 cd /home/ubuntu/test_project &&
 docker volume prune --force &&
-sudo apt-get update &&
-sudo apt-get install docker-compose &&
 COMPOSE_HTTP_TIMEOUT=200  docker-compose -f docker_compose_prod.yml up -d
