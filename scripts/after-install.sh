fe02912a4e07ea73223f2339c0e305c49edaf000
Yasin <67076345+projectslist@users.noreply.github.com>
2023-12-15 16:43:52 +0000

    Update after-install.sh

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -4,10 +4,22 @@
 cd /home/ubuntu/test_project || exit 1
 
 # Ensure the script has execute permissions
-chmod +x /opt/codedeploy-agent/deployment-root/79e5729e-d804-48b6-bae7-d6c0818b25fb/d-KUT35NR61/deployment-archive/scripts/after-install.sh
+chmod +x /opt/codedeploy-agent/deployment-root/79e5729e-d804-48b6-bae7-d6c0818b25fb/d-57361TR61/deployment-archive/scripts/after-install.sh
+
+# Function to install Docker Compose if not already installed
+install_docker_compose() {
+  if [ ! -x /usr/bin/docker-compose ]; then
+    echo "Docker Compose is not installed. Installing Docker Compose..."
+    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+    sudo chmod +x /usr/bin/docker-compose
+  fi
+}
+
+# Install Docker Compose if not already installed
+install_docker_compose
 
 # Print the PATH for debugging
 echo "Current PATH: $PATH"
 
-# Use the explicitly set full path to docker-compose to build Docker containers
+# Use the correct full path to docker-compose to build Docker containers
 /usr/bin/docker-compose -f /home/ubuntu/test_project/docker_compose_prod.yml build --no-cache
