98cdadfbf29fda81352fb70c99b1a2c0e0c50601
Yasin <67076345+projectslist@users.noreply.github.com>
2023-12-15 16:36:34 +0000

    Update after-install.sh

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -6,14 +6,9 @@ cd /home/ubuntu/test_project || exit 1
 # Ensure the script has execute permissions
 chmod +x /opt/codedeploy-agent/deployment-root/79e5729e-d804-48b6-bae7-d6c0818b25fb/d-KUT35NR61/deployment-archive/scripts/after-install.sh
 
-# Function to check if a command is available
-command_exists() {
-  command -v "$1" >/dev/null 2>&1
-}
-
 # Function to install Docker Compose if not already installed
 install_docker_compose() {
-  if ! command_exists /usr/local/bin/docker-compose; then
+  if [ ! -x /usr/local/bin/docker-compose ]; then
     echo "Docker Compose is not installed. Installing Docker Compose..."
     sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
@@ -23,8 +18,11 @@ install_docker_compose() {
 # Install Docker Compose if not already installed
 install_docker_compose
 
+# Explicitly set the PATH before running docker-compose
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/usr/local/bin:$PATH"
+
 # Print the PATH for debugging
 echo "Current PATH: $PATH"
 
-# Use the explicitly set full path to docker-compose to build Docker containers
-/usr/local/bin/docker-compose -f /home/ubuntu/test_project/docker_compose_prod.yml build --no-cache
+# Use the dynamically found docker-compose path to build Docker containers
+docker-compose -f /home/ubuntu/test_project/docker_compose_prod.yml build --no-cache
