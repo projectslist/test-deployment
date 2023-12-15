faa1e84bfc64e12c14019a59a786fcbd4ce0366e
Yasin <67076345+projectslist@users.noreply.github.com>
2023-12-15 15:38:21 +0000

    Update after-install.sh

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -4,11 +4,15 @@
 cd /home/ubuntu/test_project || exit 1
 
 # Download and install docker-compose in the project directory
-sudo curl -L "https://github.com/docker/compose/releases/download/{version}/docker-compose-$(uname -s)-$(uname -m)" -o docker-compose || exit 1
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o docker-compose || exit 1
 sudo chmod +x docker-compose || exit 1
 
 # Source the environment variables
 source /home/ubuntu/test_project/env/.env.prod
+
+# Print the contents of the .env.prod file for debugging
+echo "Contents of .env.prod:"
+cat /home/ubuntu/test_project/env/.env.prod
 
 # Build the Docker containers
 ./docker-compose -f docker_compose_prod.yml build --no-cache
